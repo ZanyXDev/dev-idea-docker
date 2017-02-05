2431a63a8c84ea1b46d6a961f25d2432a59c3f42
ZanyXDev <zanyxdev@gmail.com>
2017-02-05 07:41:05 +0300

    change to latest image

--- dev-idea-docker.sh
+++ dev-idea-docker.sh
@@ -1,12 +1,28 @@
 #!/bin/bash
-docker run -it \
+
+for dir in .idea-home .gradle .idea .android/avd IdeaProjects; do
+  mkdir -p ~/.docker-dev/$dir
+done
+
+CURRENT_DIR=`pwd`
+
+#xhost +
+#set -x
+
+docker run -tdi \
     --rm \
-    -e DISPLAY \
+    --privileged=true \
+    -e DISPLAY=${DISPLAY} \
     --net=host \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
     -v /dev/bus/usb:/dev/bus/usb \
+    -v /dev/kvm:/dev/kvm \
     -v $HOME/.Xauthority:/home/developer/.Xauthority \
-    -v $HOME/.AndroidStudio2.2:/home/developer/.AndroidStudio2.2 \
-    -v $HOME/AndroidStudioProjects:/home/developer/AndroidStuioProjects \
-    -v $HOME/persistent_data:/home/developer/data \
-    zanyxdev/dev-java-test:0.1
+    -v $CURRENT_DIR:/workspace \
+    -v $CURRENT_DIR/.docker-dev/.idea-home:/home/developer/.IdeaIC2016.3 \
+    -v $CURRENT_DIR//.android/avd:/home/developer/.android/avd \
+    -v $CURRENT_DIR/.docker-dev/IdeaProjects:/home/developer/IdeaProjects \
+    zanyxdev/dev-idea-docker:latest
+
+#set +x
+#xhost -
