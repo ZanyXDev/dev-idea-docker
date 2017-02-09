3d26af5810cee2b4d5045fd896bab85e2e5863db
ZanyXDev <zanyxdev@gmail.com>
2017-02-09 12:12:43 +0300

    remove project folder as volume

--- dev-idea-docker.sh
+++ dev-idea-docker.sh
@@ -5,8 +5,7 @@
 # /home/developer/.IdeaIC2016.3
 # /home/developer/IdeaProjects
 
-docker volume create dev_home
-docker volume create dev_home_projects
+#docker volume create dev_home
 
 docker run -tdi \
     --rm \
@@ -18,5 +17,4 @@ docker run -tdi \
     -v /dev/kvm:/dev/kvm \
     -v $HOME/.Xauthority:/home/developer/.Xauthority \
     -v dev_home:/home/developer/ \
-    -v dev_home_projects:/home/developer/IdeaProjects \    
     zanyxdev/dev-idea-docker:latest
